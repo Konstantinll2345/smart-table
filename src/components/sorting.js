7c60154694f07d22545c76ecbd13af1ed7c121db
Konstantin Shvagerius <konstantinshvagerius@yandex.ru>
2025-11-16 20:55:08 +0300

    проект 7

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,21 +1,22 @@
 import {sortCollection, sortMap} from "../lib/sort.js";
 
 export function initSorting(columns) {
-    return (data, state, action) => {
+    return (query, state, action) => {
         let field = null;
         let order = null;
 
         if (action && action.name === 'sort') {
             // @todo: #3.1 — запомнить выбранный режим сортировки
-            action.dataset.value = sortMap[action.dataset.value];
-            field = action.dataset.field;
-            order = action.dataset.value;
+            action.dataset.value = sortMap[action.dataset.value];    // Сохраним и применим как текущее следующее состояние из карты
+            field = action.dataset.field;                            // Информация о сортируемом поле есть также в кнопке
+            order = action.dataset.value;                            // Направление заберём прямо из датасета для точности
+
             // @todo: #3.2 — сбросить сортировки остальных колонок
             columns.forEach(column => {                                    // Перебираем элементы (в columns у нас массив кнопок)
             if (column.dataset.field !== action.dataset.field) {    // Если это не та кнопка, что нажал пользователь
-            column.dataset.value = 'none';                        // тогда сбрасываем её в начальное состояние
+                column.dataset.value = 'none';                        // тогда сбрасываем её в начальное состояние
             }
-            }); 
+            });
         } else {
             // @todo: #3.3 — получить выбранный режим сортировки
             columns.forEach(column => {                        // Перебираем все наши кнопки сортировки
@@ -23,9 +24,10 @@ export function initSorting(columns) {
             field = column.dataset.field;            // Сохраняем в переменных поле
             order = column.dataset.value;            // и направление сортировки
             }
-            }); 
+            });
         }
+        const sort = (field && order !== 'none') ? `${field}:${order}` : null; // сохраним в переменную параметр сортировки в виде field:direction
 
-        return sortCollection(data, field, order);
+        return sort ? Object.assign({}, query, { sort }) : query; // по общему принципу, если есть сортировка, добавляем, если нет, то не трогаем query
     }
-}  
+}
--- src/main.js
+++ src/main.js
@@ -5,14 +5,18 @@ import {data as sourceData} from "./data/dataset_1.js";
 
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
-import {initPagination} from "./components/pagination.js";
+
 import {initTable} from "./components/table.js";
+import {initPagination} from "./components/pagination.js"
 import {initSorting} from "./components/sorting.js"
+import {initFiltering} from "./components/filtering.js"
+import {initSearching} from "./components/searching.js" 
+
 // @todo: подключение
 
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const api = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -35,24 +39,40 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+    let query = {}; // здесь будут формироваться параметры запроса
+    // другие apply*
+    query = applySearching(query, state, action);
+    query = applyFiltering(query, state, action);
+    query = applySorting(query, state, action);
+    query = applyPagination(query, state, action); // обновляем query
 
-    result = applyPagination(result, state, action); 
-    sampleTable.render(result)
+    const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
+
+    updatePagination(total, query); // перерисовываем пагинатор
+    sampleTable.render(items);
 }
 
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: ['header'],
+    before: ['search', 'header', 'filter'],
     after: ['pagination']
 }, render);
 
 // @todo: инициализация
-const applyPagination = initPagination(
+const applySearching = initSearching('search');
+
+const {applyFiltering, updateIndexes} = initFiltering(sampleTable.filter.elements);
+
+const applySorting = initSorting([
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
+
+
+const {applyPagination, updatePagination} = initPagination(
     sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
     (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
         const input = el.querySelector('input');
@@ -63,14 +83,16 @@ const applyPagination = initPagination(
         return el;
     }
 );
-const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
-    sampleTable.header.elements.sortByDate,
-    sampleTable.header.elements.sortByTotal
-]);
-
-
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+    const indexes = await api.getIndexes();
+
+    updateIndexes(sampleTable.filter.elements, {
+        searchBySeller: indexes.sellers
+    });
+}
+
+init().then(render);
